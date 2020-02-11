766b66d418237336944be85635af567516376faa
younu8084 <55754022+younu8084@users.noreply.github.com>
2020-02-11 20:14:13 +0530

    Update lastsuccessfulbuild.groovy

--- JENKINS/vars/lastsuccessfulbuild.groovy
+++ JENKINS/vars/lastsuccessfulbuild.groovy
@@ -1,3 +1,3 @@
 def call(job_name){
-  sh "curl -s http://18.188.152.185:8080/job/${job_name}'/lastSuccessfulBuild/api/json --user vj:11e428e94b267ffbab27fa713e2da8e6e8"
+  sh "curl -s http://18.188.152.185:8080/job/'${job_name}'/lastSuccessfulBuild/api/json --user vj:11e428e94b267ffbab27fa713e2da8e6e8"
 }
