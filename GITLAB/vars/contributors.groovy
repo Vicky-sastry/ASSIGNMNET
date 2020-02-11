3efe1086ee222cdc4c8c61f667a72074b82c330f
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 21:04:54 +0530

    Create contributors.groovy

---
+++ GITLAB/vars/contributors.groovy
@@ -0,0 +1,5 @@
+def call(){
+//sh "curl -X GET -i -H  -d https://gitlab.com/api/v4/projects/16837979/repository/commits"
+def prs=sh "curl -s https://gitlab.com/api/v4/projects/16837979/repository/contributors --user Priyakumar:przqBFp-r6X7K_3okz5v -o ouput.json"
+  echo "$prs"
+}
