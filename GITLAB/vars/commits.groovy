ac6ce2e9597197070a750c5b8991d26bb946e92d
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 19:21:11 +0530

    Update commits.groovy

--- GITLAB/vars/commits.groovy
+++ GITLAB/vars/commits.groovy
@@ -1,4 +1,5 @@
 def call(){
 //sh "curl -X GET -i -H  -d https://gitlab.com/api/v4/projects/16837979/repository/commits"
-sh "curl -s https://gitlab.com/api/v4/projects/16837979/repository/commits --user Priyakumar:przqBFp-r6X7K_3okz5v"
+def prs=sh "curl -s https://gitlab.com/api/v4/projects/16837979/repository/commits --user Priyakumar:przqBFp-r6X7K_3okz5v -o ouput.json"
+  echo "$prs"
 }
