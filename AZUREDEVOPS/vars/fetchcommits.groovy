4eaa594b2900ffaf8bd08e87485caeceb3403814
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-02-11 17:55:54 +0530

    Update fetchcommits.groovy

--- AZUREDEVOPS/vars/fetchcommits.groovy
+++ AZUREDEVOPS/vars/fetchcommits.groovy
@@ -8,9 +8,8 @@ def resultJson = jsonSlurper.parse(reader)
 def projectId = resultJson.id
 sh """
 curl --location --request GET 'https://dev.azure.com/{orgname}/${projectName}/_apis/git/pullrequests?api-version=5.1' \
---header 'Accept: application/json' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206enN4YXBrajN6d2s2cnR6N3ptNHR5bGk3YXlrN3l0NXllaHA1aWM3ZXJsZWM0eHNmN3R5YQ=='
--H 'content-type: application/json'  | json_reformat
+
 """
 }
 
