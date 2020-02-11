787ca00532861b334279d0a5242f79559d91657b
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 18:58:05 +0530

    Create commits.groovy

---
+++ GITLAB/vars/commits.groovy
@@ -0,0 +1,4 @@
+def call(){
+//sh "curl -X GET -i -H  -d https://gitlab.com/api/v4/projects/16837979/repository/commits"
+sh "curl -s https://gitlab.com/api/v4/projects/16837979/repository/commits --user Priyakumar:przqBFp-r6X7K_3okz5v"
+}
