dd04cb0aac647aa4d3657a49efe5a06f28b24e74
Priyakumaraguru <55678173+Priyakumaraguru@users.noreply.github.com>
2020-02-11 20:06:41 +0530

    Update status.groovy

--- NEXUS/vars/status.groovy
+++ NEXUS/vars/status.groovy
@@ -5,8 +5,8 @@ def jsonObj = readJSON text: jsonString
 
 String b=jsonObj.brm.repositories.repository.id
 String repoid=b.replaceAll("\\[", "").replaceAll("\\]","");
-    withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
-         sh 'curl -X GET -i -H "Accept:application/json" -d  -u $username:$password "http://3.15.18.214:8081/nexus/service/local/repositories/${repoid}/status" '
+   // withCredentials([usernamePassword(credentialsId: 'nexus_cred', passwordVariable: 'password', usernameVariable:'username')]) {
+       sh'  curl -X GET -i -H "Accept:application/json" -d  -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/repositories/${repoid}/status" '
  
 }
 }
