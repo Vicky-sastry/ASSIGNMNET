25023b0b8259797f147aeab7b00927a15e84a6aa
ramyahd <55663832+ramyahd@users.noreply.github.com>
2020-02-12 12:46:23 +0530

    Update Jira_collect_issue.groovy

--- JIRA/vars/Jira_collect_issue.groovy
+++ JIRA/vars/Jira_collect_issue.groovy
@@ -1,9 +1,6 @@
 import groovy.json.* 
 
-
-
-
-def call(jsondata){
+def issue(jsondata){
 def jsonString = jsondata
 //println(jsonString)
 def jsonObj = readJSON text: jsonString
@@ -14,13 +11,41 @@ String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
   
 env.name = projectName
 
-def process=sh """curl -X GET \
+def process=sh """curl  -X GET \
  'http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${projectName}%20order%20by%20duedate&fields=id%2Ckey' \
   -H 'accept: application/json' \
   -H 'authorization: Basic cmlnOmRpZ2l0YWxyaWdAMTIz' \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' -o ouput.json
+  
  
 """
- echo "$process"
+ 
+} 
+
+@NonCPS
+create(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def total = resultJson.total
+  echo "$total"
+def STATUS=sh """curl -w '%{http_code}' -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'jira issues=${total}' 
+"""
+  if ( '$STATUS' == "204" )
+  {  
+  echo "Got 204! All done!"
+  }
+  else
+  {
+    echo "Got $STATUS :( Not done yet..."
+  }
+  
+ }
+
+def call()
+{
+create()
 }
+ 
+return this
