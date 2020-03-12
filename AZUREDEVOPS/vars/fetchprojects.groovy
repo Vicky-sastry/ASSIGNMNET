38b689bf962bd3e9c65c537a80ff8f6e6105fa40
Vicky-sastry <55586089+Vicky-sastry@users.noreply.github.com>
2020-03-12 16:46:51 +0530

    Update fetchprojects.groovy

--- AZUREDEVOPS/vars/fetchprojects.groovy
+++ AZUREDEVOPS/vars/fetchprojects.groovy
@@ -15,7 +15,7 @@ JSON.("name":val,"id":id)
 }
 println(JSON)
  sh """
- curl --location --request GET 'https://dev.azure.com/{orgname}/_apis/projects?api-version=5.0%0A' \
+ curl --location --request GET 'https://dev.azure.com/${orgname}/_apis/projects?api-version=5.0%0A' \
 --header 'Authorization: Basic dmlja3lzYXN0cnkudnNAb3V0bG9vay5jb206eDIyYXpoejRweHBzbmltMjJod295dzJkNG9xdjZtbzJ3czRsemgyNzZpc2trdW5ueXR5YQ=='
 """
 }
